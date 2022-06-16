67c6dd27777ec9677509835e8dd4135a64fb6265
Igwe Uchennae Felix <fesimaxu30@gmail.com>
2022-06-16 22:54:57 +0100

    update commit

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-const likeBtn = document.querySelectorAll("#like_btn")
+const likeBtns = document.querySelectorAll(".like_btn");
+console.log(likeBtns)
 
-likeBtn.forEach(likeBtn =>{
-    likeBtn.addEventListener("click", () => {
-        if (!clicked) {
-          clicked = true;
-          likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
-          count.textContent++;
-        } else {
-          clicked = false;
-          likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
-          count.textContent--;
-        }
-      });
+let likeIcon = document.querySelectorAll("#icon");
+console.log(likeIcon)
+  count = document.querySelectorAll("#count");
+  console.log(count)
+
+  let clicked = false;
+  var num=0;
+
+likeBtns.forEach(like_btn => {
+
+  like_btn.addEventListener("click", (event) => {
+
+    if (!clicked) {
+      clicked = true;
+      likeIcon.forEach(icon => {
+        icon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+      })
+      count.forEach(count => {
+        count.textContent++;
+      })
+
+      num = num+1
+      console.log("total"+num)
+
+    } else {
+      clicked = false;
+      likeIcon.forEach(icon => {
+        icon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+    })
+    count.forEach(count => {
+      count.textContent--;
+    })
+    }
+  });
+
 })
+
+// likeBtns.addEventListener("click", () => {
+//     if (!clicked) {
+//       clicked = true;
+//       likeIcon.forEach(icon => {
+//         icon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+//       })
+//       count.forEach(count => {
+//         count.textContent++;
+//       })
+
+//       num = num+1
+//       console.log("total"+num)
+
+//     } else {
+//       clicked = false;
+//       likeIcon.forEach(icon => {
+//         icon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+//     })
+//     count.forEach(count => {
+//       count.textContent--;
+//     })
+//     }
+//   });
+
+
 

f40d514875c6477bdd656788088e2a3b75d5be53
Igwe Uchennae Felix <fesimaxu30@gmail.com>
2022-06-17 03:38:45 +0100

    last commit

--- app.js
+++ app.js
@@ -38,29 +38,3 @@ likeBtns.forEach(like_btn => {
 
 })
 
-// likeBtns.addEventListener("click", () => {
-//     if (!clicked) {
-//       clicked = true;
-//       likeIcon.forEach(icon => {
-//         icon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
-//       })
-//       count.forEach(count => {
-//         count.textContent++;
-//       })
-
-//       num = num+1
-//       console.log("total"+num)
-
-//     } else {
-//       clicked = false;
-//       likeIcon.forEach(icon => {
-//         icon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
-//     })
-//     count.forEach(count => {
-//       count.textContent--;
-//     })
-//     }
-//   });
-
-
-

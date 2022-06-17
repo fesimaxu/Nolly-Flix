b17541fa1e8e01766f9b0769fb2124adae685a7b
Igwe Uchennae Felix <fesimaxu30@gmail.com>
2022-06-17 05:11:46 +0100

    functionality updated

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
+// javascript funcctionality of the button
+
+// getting the values of the button by class
 const likeBtns = document.querySelectorAll(".like_btn");
-console.log(likeBtns)
+
+// getting the values of the button by id
+//like icons
 
 let likeIcon = document.querySelectorAll("#icon");
-console.log(likeIcon)
-  count = document.querySelectorAll("#count");
-  console.log(count)
+
+// individual counts
+let count = document.querySelectorAll("#count");
 
   let clicked = false;
   var num=0;
 
+// add eventlister to first buttion to increase count 
 likeBtns.forEach(like_btn => {
 
   like_btn.addEventListener("click", (event) => {
-
-    if (!clicked) {
-      clicked = true;
-      likeIcon.forEach(icon => {
-        icon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+  
+      if (!clicked) {
+        clicked = true;
+        likeIcon.forEach(icon => {
+          icon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+        })
+        count.forEach(count => {
+          count.textContent++;
+        })
+  
+        num = num+1
+        console.log("total"+num)
+  
+      } else {
+        clicked = false;
+        likeIcon.forEach(icon => {
+          icon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
       })
       count.forEach(count => {
-        count.textContent++;
+        count.textContent--;
       })
-
-      num = num+1
-      console.log("total"+num)
-
-    } else {
-      clicked = false;
-      likeIcon.forEach(icon => {
-        icon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
-    })
-    count.forEach(count => {
-      count.textContent--;
-    })
-    }
-  });
-
-})
-
+      }
+    });
+  
+  })
+  
